a439376223248aca4c5ff7a3036326e83a6a9919
ShrodPas <ethanshrod@gmail.com>
2025-11-11 21:17:58 +0800

    Made html code cleaner and more readable

--- Scripts/ctof.js
+++ Scripts/ctof.js
@@ -1,6 +1,6 @@
 function convert() {
     let celsius = document.getElementById("temp").value;
     let fahrenheit = (celsius * 9 / 5) + 32;
-    document.getElementById("display").innerHTML = 
+    document.getElementById("display").innerHTML =
         `${celsius}°C = ${fahrenheit.toFixed(2)}°F`;
 }
