9d4925460339ae653b1429b30cf0c0110f4d7914
ShrodPas <ethanshrod@gmail.com>
2025-11-11 13:50:19 +0800

    Added function to feet to meters

---
+++ Scripts/ftom.js
@@ -0,0 +1,6 @@
+function convert () {
+    let feet = document.getElementById("Feet").value*1;
+    let meters = feet / 3.28084;
+    document.getElementById("display").innerHTML = feet + " to Meters is = " + meters.toFixed(2) +"m";
+
+}
