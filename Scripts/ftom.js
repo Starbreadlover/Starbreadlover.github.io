7b4fccf0e15c73310a6d38ee51b4c7232fb55358
ShrodPas <ethanshrod@gmail.com>
2025-11-12 23:44:31 +0800

    New styles for conversion assignments and cleaner index styles css

--- Scripts/ftom.js
+++ Scripts/ftom.js
@@ -1,6 +1,6 @@
 function convert () {
     let feet = document.getElementById("Feet").value*1;
     let meters = feet / 3.28084;
-    document.getElementById("display").innerHTML = feet + " to Meters is = " + meters.toFixed(2) +"m";
+    document.getElementById("display").innerHTML = feet + " Feet to Meters is = " + meters.toFixed(2) +"m";
 
 }
